2735b747d632e7075d797392f4d6bbd06df68811
Andresito126 <creed126ax@gmail.com>
2024-09-29 13:02:14 -0600

    Add: Service Created, data-pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePComponent } from './home/home-p/home-p.component';
+import { PokemonPComponent } from './pokemons/pokemon-p/pokemon-p.component';
 
 const routes: Routes = [
-  {path:'', component:HomePComponent}
+  {path:'', component:HomePComponent},
+  {path: 'pokemons', component:PokemonPComponent}
 ];
 
 @NgModule({
