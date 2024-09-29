84512638af349e92128de0c1ffbbae44f06da876
Andresito126 <creed126ax@gmail.com>
2024-09-28 21:53:29 -0600

    Add: Adding styles to the component section-pokemons. Add:Adding Imgs for the section-pokemon

---
+++ src/app/home/section-pokemons/section-pokemons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-pokemons',
+  templateUrl: './section-pokemons.component.html',
+  styleUrl: './section-pokemons.component.css'
+})
+export class SectionPokemonsComponent {
+
+}
