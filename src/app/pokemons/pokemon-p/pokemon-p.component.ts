7c9db0eb3b9d85f93a8bc15ff504983573cde993
Andresito126 <creed126ax@gmail.com>
2024-09-30 00:37:23 -0600

    Add: Getting the first 10 pokemons

--- src/app/pokemons/pokemon-p/pokemon-p.component.ts
+++ src/app/pokemons/pokemon-p/pokemon-p.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataPokemonService } from '../../services/data-pokemon.service';
 
 @Component({
   selector: 'app-pokemon-p',
   templateUrl: './pokemon-p.component.html',
   styleUrl: './pokemon-p.component.css'
 })
-export class PokemonPComponent {
+export class PokemonPComponent implements OnInit {
 
-}
+  constructor(private dataPokemonService: DataPokemonService){}
+
+  ngOnInit(): void {
+      this.dataPokemonService.getPokemons().subscribe((response:any)=>{
+        console.log(response);
+      });
+
+      }
+  }
+
+
