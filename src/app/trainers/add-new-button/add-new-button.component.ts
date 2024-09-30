dbc75f21b6f26044e54a1b70cba4a4159ab10806
Andresito126 <creed126ax@gmail.com>
2024-09-30 15:48:11 -0600

    Add: add-button component created

---
+++ src/app/trainers/add-new-button/add-new-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-button',
+  templateUrl: './add-new-button.component.html',
+  styleUrl: './add-new-button.component.css'
+})
+export class AddNewButtonComponent {
+
+}
--- src/app/trainers/trainers.module.ts
+++ src/app/trainers/trainers.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainersPComponent } from './trainers-p/trainers-p.component';
+import { AddNewButtonComponent } from './add-new-button/add-new-button.component';
 
 
 
 @NgModule({
   declarations: [
-    TrainersPComponent
+    TrainersPComponent,
+    AddNewButtonComponent
   ],
   imports: [
     CommonModule
