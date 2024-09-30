c54e1e4b6240425e33e21da9c0ed480090470fd1
Andresito126 <creed126ax@gmail.com>
2024-09-30 00:48:02 -0600

    Add: Getting more stats from the pokemons

--- src/app/pokemons/pokemon-p/pokemon-p.component.ts
+++ src/app/pokemons/pokemon-p/pokemon-p.component.ts
@@ -7,12 +7,20 @@ import { DataPokemonService } from '../../services/data-pokemon.service';
   styleUrl: './pokemon-p.component.css'
 })
 export class PokemonPComponent implements OnInit {
+  pokemons: any[] =[];
 
   constructor(private dataPokemonService: DataPokemonService){}
 
   ngOnInit(): void {
-      this.dataPokemonService.getPokemons().subscribe((response:any)=>{
-        console.log(response);
+      this.dataPokemonService.getPokemons()
+      .subscribe((response:any)=>{
+        response.results.forEach((result: { name: string; })=>{
+          this.dataPokemonService.getDataPokemon(result.name)
+           .subscribe((uniqResponse:any)=>{
+            this.pokemons.push(uniqResponse);
+            console.log(this.pokemons);
+          });
+        });
       });
 
       }
