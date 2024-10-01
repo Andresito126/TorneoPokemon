c95e1c22a4322e74175d4fc3eccdf0c8d3ff7119
Andresito126 <creed126ax@gmail.com>
2024-10-01 17:26:02 -0600

    Changing the path for the modals trainers

--- src/app/trainers/trainers.module.ts
+++ src/app/trainers/trainers.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainersPComponent } from './trainers-p/trainers-p.component';
 import { AddNewButtonComponent } from './add-new-button/add-new-button.component';
+import { AddModalComponent } from './modals/add-modal/add-modal.component';
+import { EditModalComponent } from './modals/edit-modal/edit-modal.component';
+import { DeleteModalComponent } from './modals/delete-modal/delete-modal.component';
 
 
 
 @NgModule({
   declarations: [
     TrainersPComponent,
-    AddNewButtonComponent
+    AddNewButtonComponent,
+    AddModalComponent,
+    EditModalComponent,
+    DeleteModalComponent
   ],
   imports: [
     CommonModule
