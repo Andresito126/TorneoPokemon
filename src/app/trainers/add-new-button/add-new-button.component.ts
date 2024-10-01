9543242bdd4bca4ad149529c6b7e039efc4b6fe2
Andresito126 <creed126ax@gmail.com>
2024-10-01 16:22:34 -0600

    passing data from trainers to html table

--- src/app/trainers/add-new-button/add-new-button.component.ts
+++ src/app/trainers/add-new-button/add-new-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-new-button',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AddNewButtonComponent {
 
+  @Input() nameButton:string = "";
 }
--- src/app/trainers/trainers-p/trainers-p.component.ts
+++ src/app/trainers/trainers-p/trainers-p.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TrainerService } from '../services/trainer.service';
+import { Trainer } from '../models/trainer';
 @Component({
   selector: 'app-trainers-p',
   templateUrl: './trainers-p.component.html',
   styleUrl: './trainers-p.component.css'
 })
-export class TrainersPComponent {
+export class TrainersPComponent implements OnInit {
+  trainers: Trainer[] = []; 
 
+  constructor(private trainerService: TrainerService) { }
+
+  ngOnInit(): void {
+    this.getTrainers();
+  }
+
+  getTrainers(): void {
+    this.trainerService.getAllTrainers().subscribe(
+      (data: Trainer[]) => {
+        this.trainers = data; 
+      },
+      error => {
+        console.error('Error al obtener entrenadores:', error);
+      }
+    );
+
+}
 }
