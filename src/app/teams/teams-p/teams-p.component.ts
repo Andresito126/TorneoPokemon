842d6935eb271ef29e7ef79753e766c09ca30da5
Andresito126 <creed126ax@gmail.com>
2024-10-02 13:53:44 -0600

    Add:  principal component for teams created

---
+++ src/app/teams/teams-p/teams-p.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teams-p',
+  templateUrl: './teams-p.component.html',
+  styleUrl: './teams-p.component.css'
+})
+export class TeamsPComponent {
+
+}
--- src/app/teams/teams.module.ts
+++ src/app/teams/teams.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamsPComponent } from './teams-p/teams-p.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamsPComponent
+  ],
   imports: [
     CommonModule
   ]
